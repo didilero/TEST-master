f9fdd05bb7d5c3e0dc5b9e92f0e36d638dca1d95
didilero <didileroVA1999@hotmail.fr>
2018-09-24 10:38:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+
+	private static Scanner clavier;
+	public static void main(String[] args) {
+		clavier = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		int nb;
+		System.out.println("saisir :");
+		
+		try{	
+		nb = clavier.nextInt();
+		}
+		catch(Exception erreur) {			
+			System.out.println(erreur.toString());
+		}
+
+	//ligne de commentaire
+	//deuxieme ligne de commentaire
+	//ho djadja ,genre en catchana baby tu dead ca
+	// Oh djadja !
+	// Changer par didilERO
+	}
+}
